8094798e090fac5a7e2d5d3d4ca6b692f4aaf3fc
github username <github email address>
2021-03-10 04:18:20 -0600

    commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,8 @@ class User < ApplicationRecord
   
     has_secure_password
   
-    validates :email, :password, :first_name, :last_name, presence: true
-    validates :password, length: {minimum: 3}
+    # validates :email, :password, :first_name, :last_name, presence: true
+    # validates :password, length: {minimum: 3}
     # validates :username, :password, :email, uniqueness: { case_sensitive: false }
 
 end
