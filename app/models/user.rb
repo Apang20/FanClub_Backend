f3ee6e5f1d258cb6487ca14b61e70339724e3b8f
github username <github email address>
2021-03-10 10:24:15 -0600

    commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,17 +2,18 @@ class User < ApplicationRecord
 
     has_many :carts
     has_many :cart_items, through: :carts
-    #u1=User.all.first
-    #u1.carts (line3)
-    #u1.cart_itmes (line4)
+    
+    has_secure_password
 
     # has_many :cart_items
     # has_many :items, through: :cart_items
-  
-    has_secure_password
-  
+    
+    
     # validates :email, :password, :first_name, :last_name, presence: true
     # validates :password, length: {minimum: 3}
     # validates :username, :password, :email, uniqueness: { case_sensitive: false }
-
+    
+    #u1=User.all.first
+    #u1.carts (line3)
+    #u1.cart_itmes (line4)
 end
