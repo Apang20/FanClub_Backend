cce362405076d4afc41376c2a76f24e0c35c8f22
github username <avelonpang@gmail.com>
2021-04-13 11:51:11 -0500

    updated cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://ghibli-fanclub-store.herokuapp.com/'
+    origins 'https://powerful-island-44705.herokuapp.com/'
 
     resource '*',
       headers: :any,
