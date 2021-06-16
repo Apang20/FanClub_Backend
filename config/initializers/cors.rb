a5bdc620a41a07b64899a71ad17a35b97373d8eb
github username <avelonpang@gmail.com>
2021-06-16 09:35:45 -0500

    update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://powerful-island-44705.herokuapp.com/'
+    origins '*'
+    # origins 'https://powerful-island-44705.herokuapp.com/'
 
     resource '*',
       headers: :any,
